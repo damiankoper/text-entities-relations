0a9590042fd0c73a86d9012ce487133bea6f4b4a
damiankoper <kopernickk@gmail.com>
2021-05-30 16:17:21 +0200

    feat: filter tooltips

--- packages/front/src/components/Filter.vue
+++ packages/front/src/components/Filter.vue
@@ -1,25 +1,48 @@
 <template>
   <el-form>
     <el-container direction="vertical">
-      <el-form-item label="Nazwa" label-width="120px">
-        <el-select
-          no-data-text="Wpisz nazwę"
-          multiple
-          filterable
-          allow-create
-          default-first-option
-          v-model="params.name.values"
-          placeholder="Nazwa wierzchołka"
-        >
-        </el-select>
-      </el-form-item>
-      <el-form-item label="Max. odległość" label-width="120px">
-        <el-input-number v-model="params.name.maxDistance" :min="0" />
-      </el-form-item>
+      <el-tooltip placement="left">
+        <template #content>
+          <div style="max-width:250px">
+            Pokaż wierzchołki, które zawierają podane ciągi znaków, których
+            odległość Levenshteina pomiędzy nazwą wierzchołka nie przekszacza
+            podanej liczby.
+          </div>
+        </template>
+        <div style="margin-bottom:-22px">
+          <el-form-item label="Nazwa" label-width="120px">
+            <el-select
+              no-data-text="Wpisz nazwę"
+              multiple
+              filterable
+              allow-create
+              default-first-option
+              v-model="params.name.values"
+              placeholder="Nazwa wierzchołka"
+            >
+            </el-select>
+          </el-form-item>
+          <el-form-item label="Max. odległość" label-width="120px">
+            <el-input-number
+              v-model="params.name.maxDistance"
+              :min="0"
+              :disabled="!params.name.values.length"
+            />
+          </el-form-item>
+        </div>
+      </el-tooltip>
       <el-divider />
-      <el-form-item label="Min. waga" label-width="120px">
-        <el-input-number v-model="params.minWeight" :min="0" />
-      </el-form-item>
+      <el-tooltip placement="left">
+        <template #content>
+          <div style="max-width:250px">
+            Pokaż wierzchołki, które mają tyle samo lub więcej relacji niż
+            podana liczba.
+          </div>
+        </template>
+        <el-form-item label="Minimum relacji" label-width="120px">
+          <el-input-number v-model="params.minWeight" :min="0" />
+        </el-form-item>
+      </el-tooltip>
       <el-row :gutter="16" type="flex" justify="end">
         <el-col :span="8">
           <el-button style="width: 100%" type="primary" @click="reset" plain>
